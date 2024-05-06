6f2a995e9bcdc311149c993c0251bdb17fb195ee
Ross Laing <30736276+RossLaing8417@users.noreply.github.com>
2024-05-06 10:41:27 +0200

    feat: cli module

--- build.zig
+++ build.zig
@@ -22,6 +22,12 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    _ = b.addModule("cli", .{
+        .root_source_file = b.path("src/cli.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
     // step when running `zig build`).
---
+++ src/cli.zig
@@ -0,0 +1,4 @@
+const Binding = @import("binding.zig");
+
+pub const Command = @import("command.zig");
+pub const bind = Binding.bind;
